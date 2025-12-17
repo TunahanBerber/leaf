0dbf7117c25a40b4fb60f35629e9d3a7d6c5886a
tunahan.berber <tunahan.berber@eba.gov.tr>
2025-12-17 17:43:38 +0300

    add project files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <div style={{ padding: 32 }}>
+        <h1>Leaf 🌿</h1>
+        <p>Personal digital library for physical readers</p>
+      </div>
+    </>
+  )
+}
+
+export default App
